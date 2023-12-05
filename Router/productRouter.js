b904c224aee1009d7bcea9579ca96f9da6c66967
Anshuman Kumar <mahianshuman491@gmail.com>
2023-12-05 17:00:59 +0530

    product routing update

--- Router/productRouter.js
+++ Router/productRouter.js
@@ -1,9 +1,12 @@
 const route = require("express").Router();
+const { productCollection } = require("../model/productModel");
 
-const { fetchData } = require("../controller/getAllData");
 
 
-route.get("/product", fetchData);
+route.get("/product", async (request,response)=>{
+    let items = await productCollection.find();
+    return response.send(items);
+});
 
 
 module.exports = route
--- controller/getAllData.js
+++
@@ -1,8 +0,0 @@
-const { productCollection } = require("../model/productModel");
-
-const fetchData = async (request, response) => {
-    let items = await productCollection.find();
-    return response.send(items);
-}
-
-module.exports = { fetchData }
