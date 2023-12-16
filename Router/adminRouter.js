454d64ffa077af37be91d70b576d614fc308f7ee
Anshuman Kumar <mahianshuman491@gmail.com>
2023-12-16 11:28:52 +0530

    code deleted

--- Router/adminRouter.js
+++ Router/adminRouter.js
@@ -2,12 +2,10 @@ const route = require("express").Router();
 const { addnewProduct } = require("../controller/AddNewProduct");
 const { adminLogin } = require("../controller/AdminLogin")
 const { deleteProduct } = require("../controller/DeleteProduct")
-const { adminPasswordUpdate } = require("../controller/PasswordUpdateCtrl")
 const { getAllUsers } = require("../controller/GetAllUserCtrl")
 
 
 route.post("/login", adminLogin)
-route.post("/update-passoword", adminPasswordUpdate)
 route.post("/add-product", addnewProduct)
 route.post("/deleteproduct/:id", deleteProduct)
 route.get("/allUsers", getAllUsers)
