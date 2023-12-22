b95ccb990fff1c734f6dc677c0b7cd4cd868daae
Anshuman Kumar <mahianshuman491@gmail.com>
2023-12-22 16:22:06 +0530

    user router updated

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,9 +1,16 @@
 const route = require("express").Router();
 const { userLogin } = require("../controller/userLogin");
 const {userRegister} = require("../controller/userRegister")
+const { cartCollection } = require("../model/cartModel");
 
 
 
 route.post("/register", userRegister)
-route.post("/login", userLogin)
+route.post("/login", userLogin);
+route.get("/cartItems/:userEmail", async(request, response)=>{
+    const {userEmail} = request.params
+    const mongooseResponse = await cartCollection.find({userEmail : {$eq : userEmail}})
+    return response.send(mongooseResponse)
+
+})
 module.exports = route
