33c139815f7e7e4de88e9fc7c0030cc22686dfff
Anshuman Kumar <mahianshuman491@gmail.com>
2023-12-28 13:02:36 +0530

    product router updated

--- Router/productRouter.js
+++ Router/productRouter.js
@@ -20,18 +20,6 @@ route.get("/products/:id", async (request, response) => {
     response.send(singleProduct);
 });
 
-route.post('/addtowishlist', async (request, response) => {
-    let tempItem = request.body
-    const mongooseRes = await wishListCollection.create(tempItem);
-    return response.send(mongooseRes)
-});
-
-route.post("/removetowishlist", async (request, response)=>{
-    let tempItem = request.body
-    const mongooseRes = await wishListCollection.findOneAndDelete({ $and: [{ id: {$eq : tempItem.id} }, { userEmail: { $eq: tempItem.userEmail} }] });
-    return response.send(mongooseRes)
-})
-
 
 
 module.exports = route
