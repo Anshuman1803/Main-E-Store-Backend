7e18274e139cb03dd68a2c0c4c9c4eb1d11c5ac5
Anshuman Kumar <mahianshuman491@gmail.com>
2024-01-10 00:08:04 +0530

    user update password

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,4 +1,5 @@
 const route = require("express").Router();
+const { updateUserPassword } = require("../controller/updateUserPassword");
 const { userLogin } = require("../controller/userLogin");
 const {userRegister} = require("../controller/userRegister")
 const { cartCollection } = require("../model/cartModel");
@@ -7,10 +8,13 @@ const { cartCollection } = require("../model/cartModel");
 
 route.post("/register", userRegister)
 route.post("/login", userLogin);
+
 route.get("/cartItems/:userEmail", async(request, response)=>{
     const {userEmail} = request.params
     const mongooseResponse = await cartCollection.find({userEmail : {$eq : userEmail}})
     return response.send(mongooseResponse)
 
-})
+});
+
+route.patch("/update-password", updateUserPassword);
 module.exports = route
---
+++ controller/updateUserPassword.js
@@ -0,0 +1,46 @@
+const bcrypt = require("bcrypt");
+const saltRound = 10;
+const registredUserCollection = require("../model/userModel");
+
+const updateUserPassword = async (request, response) => {
+    const tempUser = request.body;
+
+    if (tempUser.validUser) {
+        //hashing password using bcrypt
+        tempUser.userPassword = bcrypt.hashSync(tempUser.userPassword, saltRound);
+
+        let mongooseResponse = await registredUserCollection.updateOne({ userEmail: tempUser.userEmail }, { userPassword: tempUser.userPassword });
+
+        if (mongooseResponse.acknowledged) {
+
+            response.send({ resMsg: "Password Updated" })
+        } else {
+            response.send({ resMsg: "Something Went Wrong." })
+        }
+    } else {
+
+        let IsRegistred = await registredUserCollection.findOne({ userEmail: { $eq: tempUser.userEmail } });
+
+        if (IsRegistred) {
+            
+            //hashing password using bcrypt
+            tempUser.userPassword = bcrypt.hashSync(tempUser.userPassword, saltRound);
+
+            let mongooseResponse = await registredUserCollection.updateOne({ userEmail: tempUser.userEmail }, { userPassword: tempUser.userPassword });
+
+            if (mongooseResponse.acknowledged) {
+                response.send({ resMsg: "Password Updated" })
+            } else {
+                response.send({ resMsg: "Something Went Wrong." })
+            }
+
+        } else {
+            return response.send({ resMsg: "User Not Registred" });
+        }
+
+    }
+
+
+}
+
+module.exports = { updateUserPassword }
